c706fc447e0e51a34a70d291ad9a662edddbffa3
ribaricplusplus <ribaricplusplus@users.noreply.github.com>
2019-11-27 18:35:44 +0100

    Implemented LtpError

---
+++ src/errors/ltp_error.js
@@ -0,0 +1,27 @@
+/**
+ * Error designed to make user-reporting convenient by just needing to print the message. For debugging purposes,
+ * check [cause]{@link module:Errors.LtpError#cause}.
+ * @class
+ * @memberof module:Errors
+ */
+class LtpError{
+    /**
+     * 
+     * @param {Error} err Any error that was thrown within LTP library.
+     */
+    constructor(err){
+        /** Error representing what exactly happened within LTP. */
+        this.cause = err;
+        if (["CleanerError", "BracketedError", "SyntaxError"].includes(err.name)) {
+            if (err.name == "SyntaxError") {
+                const CONTEXT_LENGTH = 10;
+                const context = inputText.substring(err.location.start.offset, err.location.end.offset + CONTEXT_LENGTH + 1);
+                err.message = `Unexpected token "${err.found}" at line ${err.location.start.line}, column ${err.location.start.column}, within context: ${context}`;
+            }
+            err.message = `Error: ${err.message}`;
+        } else {
+            err.message = `Error.`
+        }
+        this.name = "LtpError";
+    }
+}
--- src/ltp.js
+++ src/ltp.js
@@ -5,17 +5,19 @@
 const cleaner = require("./cleaner");
 const translator = require("./translator");
 const parser = require("./parser");
+const LtpError = require("./errors/ltp_error");
 /**
  * Main entry point of the library.
  * Converts a LaTeX expression into the syntax that the Photomath solver understands.
- * Returns a user-friendly error message in case the LaTeX expression is invalid.
+ * Throws a user-friendly error message in case the LaTeX expression is invalid.
  *
  * @param {String} inputText Latex expression.
  * @returns {String} Photomath solver syntax representation of inputText, or error message.
+ * @throws {LtpError} Error that can be identified by having the name LtpError. 
  */
 function ltp(inputText) {
     let cleaned; let parsed; let
-translated;
+        translated;
     try {
         cleaned = cleaner.clean(inputText);
         parsed = parser.parse(cleaned);
@@ -29,9 +31,8 @@ translated;
                 err.message = `Unexpected token "${err.found}" at line ${err.location.start.line}, column ${err.location.start.column}, within context: ${context}`;
             }
             err.message = `Error: ${err.message}`;
-            return err.message;
         }
-            return "Error.";
+        throw new LtpError(err);
     }
 }
 
